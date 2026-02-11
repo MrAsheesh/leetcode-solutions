917de026321d81d1b7ed229d83088ac47b13a4a2
ASHEESH KUMAR <thedeveloper0407@gmail.com>
2026-02-11 10:43:14 +0530

    Add LeetCode 75 Short Colors Solution

---
+++ Arrays/75-Sort-Colors.java
@@ -0,0 +1,40 @@
+/*
+LeetCode 75 - Short Colors
+https://leetcode.com/problems/sort-colors/description/
+*/
+
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int count1 = 0;
+        int count2 = 0; 
+        int count3 = 0;
+        
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]==0) {
+                count1++;
+            }
+            else if(nums[i]==1) {
+                count2++;
+            }
+            else {
+                count3++;
+            }
+        }
+
+        for(int i=0; i<nums.length; i++) {
+            if(count1>0) {
+                nums[i] = 0;
+                count1--;
+            }
+            else if(count2>0) {
+                nums[i] = 1;
+                count2--;
+            }
+            else {            
+                nums[i] = 2;
+            }
+        }
+
+    }
+}
